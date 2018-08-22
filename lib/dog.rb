a21b4fa911ce78886823f8c91bf8eb09fa635c43
Daniel Schoch <d.schoch90@gmail.com>
2018-08-22 22:30:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,14 @@
 class Dog
   @@all = []
 
+  attr_accessor :name
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
   def self.all
-    @@all.each {|dog| puts "#{dog}" }
+    @@all.each {|dog| puts dog.name }
   end
 end
